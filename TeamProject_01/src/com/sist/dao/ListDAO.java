0d5d7c52f2eddbb0ff884f69d7583f5035813f04
sleep <sleep@DESKTOP-KDH5PAL>
2021-03-31 17:15:20 +0900

    list dao,vo

--- TeamProject_01/src/com/sist/dao/ListDAO.java
+++ TeamProject_01/src/com/sist/dao/ListDAO.java
@@ -1,5 +1,74 @@
 package com.sist.dao;
+import javax.naming.*;
+import java.sql.*;
+import javax.sql.*;
 
+import com.sist.vo.ListVO;
+
+import java.util.*;
 public class ListDAO {
 
+	private Connection conn;
+	private PreparedStatement ps;
+	
+	private static ListDAO dao;
+	
+	public void getConnection()
+	{
+		try
+		{
+			Context init=new InitialContext();
+			Context c=(Context)init.lookup("java://comp/env");
+			DataSource ds=(DataSource)c.lookup("jdbc/oracle");
+			conn=ds.getConnection();
+		}catch(Exception ex) {}
+	}
+	public void disConnection()
+	{
+		try
+		{
+			if(ps!=null) ps.close();
+			if(conn!=null) conn.close();
+		}catch(Exception ex) {}
+		
+	}
+	public static ListDAO newInstance()
+	{
+		if(dao==null)
+			dao=new ListDAO();
+		
+		return dao;
+	}
+	public List<ListVO> recipeListData()
+	{
+		List<ListVO> list = new ArrayList<ListVO>();
+		try
+		{
+			getConnection();
+			String sql="SELECT no, title, poster "
+					+ "FROM list_data_v4 "
+					+ "ORDER BY hit DESC";
+			ps=conn.prepareStatement(sql);
+			
+			ResultSet rs = ps.executeQuery();
+			while(rs.next())
+			{
+				ListVO vo = new ListVO();
+				vo.setNo(rs.getInt(1));
+				vo.setTitle(rs.getString(2));
+				vo.setPoster(rs.getString(3));
+				list.add(vo);
+			}
+			rs.close();
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}finally
+		{
+			disConnection();
+		}
+		return list;
+	}
+	
+	
 }
--- TeamProject_01/src/com/sist/vo/ListVO.java
+++ TeamProject_01/src/com/sist/vo/ListVO.java
@@ -1,5 +1,48 @@
 package com.sist.vo;
 
 public class ListVO {
-
+	private int no;
+	private String poster,title,chef_poster,chef,link,hit;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getPoster() {
+		return poster;
+	}
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getChef_poster() {
+		return chef_poster;
+	}
+	public void setChef_poster(String chef_poster) {
+		this.chef_poster = chef_poster;
+	}
+	public String getChef() {
+		return chef;
+	}
+	public void setChef(String chef) {
+		this.chef = chef;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getHit() {
+		return hit;
+	}
+	public void setHit(String hit) {
+		this.hit = hit;
+	}
 }
