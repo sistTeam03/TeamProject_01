894fe9064ae9f8e35d579e36ac83c1685beafa9a
YNotes <65722992+YNseries@users.noreply.github.com>
2021-04-03 01:49:38 +0900

    chef_recipe(수정중)

--- TeamProject_01/src/com/sist/model/ChefsModel.java
+++ TeamProject_01/src/com/sist/model/ChefsModel.java
@@ -46,7 +46,11 @@ public class ChefsModel {
 	@RequestMapping("recipe/chefs_recipe.do")
 	public String chefsRecipe(HttpServletRequest request,HttpServletResponse response)
 	{
-		
+		String no=request.getParameter("no");
+		DetailDAO dao=DetailDAO.newInstance();
+		List<DetailVO> dList=dao.chefsRecipeData(Integer.parseInt(no));
+		request.setAttribute("dList", dList);
+		request.setAttribute("main_jsp", "../recipe/chefs_recipe.jsp"); 
 		return "../main/chef.jsp";
 	}
 }
